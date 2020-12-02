2d63108328392a9ca642cb2f61f7dccbe23c2215
Mykhal Parson <mykhal.reign@mail.citytech.cuny.edu>
2020-12-02 10:12:18 -0500

    FINAL WIP

---
+++ Final_WIP/Final_WIP.ino
@@ -0,0 +1,348 @@
+
+const int LED1 = 7; // variable for which pin
+const int LED2 = 8; // variable for which pin
+const int LED3 = 9; // variable for which pin
+const int LED4 = 10; // variable for which pin
+
+const int sound = 12; //variable for speaker sound
+
+void setup() {
+  
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+  pinMode(LED3,OUTPUT);
+  pinMode(LED4,OUTPUT);
+  pinMode(sound,OUTPUT);
+  //LED Pattern with sound 
+
+    
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+    digitalWrite(7,HIGH);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 100);
+    delay(1000); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,HIGH); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 200);
+    delay(1000); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,HIGH); 
+    digitalWrite(10,LOW);
+    tone (sound, 800);
+    delay(1000); 
+
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,HIGH);
+    tone (sound, 400);
+    delay(1000); 
+    
+    digitalWrite(7,HIGH);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 100);
+    delay(800); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,HIGH); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 200);
+    delay(800); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,HIGH); 
+    digitalWrite(10,LOW);
+    tone (sound, 800);
+    delay(800); 
+
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,HIGH);
+    tone (sound, 400);
+    delay(800); 
+
+    digitalWrite(7,HIGH);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 100);
+    delay(800); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,HIGH); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 200);
+    delay(800); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,HIGH); 
+    digitalWrite(10,LOW);
+    tone (sound, 800);
+    delay(800); 
+
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,HIGH);
+    tone (sound, 400);
+    delay(800); 
+   
+    digitalWrite(7,HIGH);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 100);
+    delay(500); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,HIGH); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 200);
+    delay(500); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,HIGH); 
+    digitalWrite(10,LOW);
+    tone (sound, 800);
+    delay(500); 
+
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,HIGH);
+    tone (sound, 400);
+    delay(500); 
+
+ digitalWrite(7,HIGH);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 100);
+    delay(500); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,HIGH); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 200);
+    delay(500); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,HIGH); 
+    digitalWrite(10,LOW);
+    tone (sound, 800);
+    delay(500); 
+
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,HIGH);
+    tone (sound, 400);
+    delay(500); 
+
+    digitalWrite(7,HIGH);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 100);
+    delay(350); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,HIGH); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 200);
+    delay(350); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,HIGH); 
+    digitalWrite(10,LOW);
+    tone (sound, 800);
+    delay(350); 
+
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,HIGH);
+    tone (sound, 400);
+    delay(350); 
+
+     digitalWrite(7,HIGH);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 100);
+    delay(350); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,HIGH); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 200);
+    delay(350); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,HIGH); 
+    digitalWrite(10,LOW);
+    tone (sound, 800);
+    delay(350); 
+
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,HIGH);
+    tone (sound, 400);
+    delay(350); 
+
+    digitalWrite(7,HIGH);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 100);
+    delay(200); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,HIGH); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 200);
+    delay(200); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,HIGH); 
+    digitalWrite(10,LOW);
+    tone (sound, 800);
+    delay(200); 
+
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,HIGH);
+    tone (sound, 400);
+    delay(200); 
+
+        digitalWrite(7,HIGH);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 100);
+    delay(150); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,HIGH); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,LOW);
+    tone (sound, 200);
+    delay(150); 
+    
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,HIGH); 
+    digitalWrite(10,LOW);
+    tone (sound, 800);
+    delay(150); 
+
+    digitalWrite(7,LOW);
+    digitalWrite(8,LOW); 
+    digitalWrite(9,LOW); 
+    digitalWrite(10,HIGH);
+    tone (sound, 400);
+    delay(150); 
+
+       
+    
+
+   /* tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(750); 
+    
+    tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(1000); 
+    tone (sound, 1000);
+    delay(750); 
+
+    tone (sound, 1000);
+    delay(100); 
+    tone (sound, 1000);
+    delay(100); 
+    tone (sound, 1000);
+    delay(100); 
+    tone (sound, 1000);
+    delay(100);
+    
+    tone (sound, 1000);
+    delay(100); 
+    tone (sound, 1000);
+    delay(100); 
+    tone (sound, 1000);
+    delay(100); 
+    tone (sound, 1000);
+    delay(100);
+    
+    tone (sound, 1000);
+    delay(50); 
+    tone (sound, 1000);
+    delay(50); 
+    tone (sound, 1000);
+    delay(50); 
+    tone (sound, 1000);
+    delay(50);
+    tone (sound, 1000);
+    delay(50); 
+    tone (sound, 1000);
+    delay(50); 
+    tone (sound, 1000);
+    delay(50); 
+    tone (sound, 1000);
+    delay(50);
+   */
+}
